4dadd29aaf536f1ff7253b30062c5e2a9522a600
Felix Gateru <felix.gateru@gmail.com>
2026-03-05 19:47:17 +0300

    feat: add count.py script that counts to 1000 and back
    Signed-off-by: Felix Gateru <felix.gateru@gmail.com>

---
+++ count.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+A program that counts up to 1000 and then counts down.
+"""
+
+def main():
+    # Count up to 1000
+    print("Counting up to 1000...")
+    for i in range(1, 1001):
+        print(i)
+    
+    # Count down from 1000
+    print("\nCounting down from 1000...")
+    for i in range(1000, 0, -1):
+        print(i)
+    
+    print("\nDone!")
+
+if __name__ == "__main__":
+    main()

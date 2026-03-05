03884dcc8676132f9cb0e650a9a490d46a1532ac
Felix Gateru <felix.gateru@gmail.com>
2026-03-05 21:22:26 +0300

    refactor: increment to 1001
    Signed-off-by: Felix Gateru <felix.gateru@gmail.com>

--- count.py
+++ count.py
@@ -6,12 +6,12 @@ A program that counts up to 1000 and then counts down.
 def main():
     # Count up to 1000
     print("Counting up to 1000...")
-    for i in range(1, 1001):
+    for i in range(1, 1002):
         print(i)
     
     # Count down from 1000
     print("\nCounting down from 1000...")
-    for i in range(1000, 0, -1):
+    for i in range(1001, 0, -1):
         print(i)
     
     print("\nDone!")
